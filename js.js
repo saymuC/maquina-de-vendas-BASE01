a1ac5e14ca6f3c4be7e132df7e328d72a3fc2acb
saymu_c <saymonhenrique2374@gmail.com>
2025-04-14 01:55:08 -0400

    adição de um sistema de historico
    sistema que salva informações sobre o cliente em historico.json

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const fs = require('fs');
 const produtos1 = ["Coca", "Fanta", "Pepsi", "Guarana"];
 const produtos2 = ["Militos", "Cheetos", "Doritos", "Fandangos"];
 const produtos3 = ["Barra de Chocolate", "Barra de proteina", "Barrinha de cereal", "Barrinha de Flocos"];
@@ -14,6 +15,7 @@ console.log('Olá ' + nome + '!');
 if (readline.keyInYN('Voce deseja comprar algum de nossos produtos?')) {
     console.clear();
     const escolha = readline.keyInSelect(categoria, 'Escolha uma dessas categorias!');
+    const categoria_escolha = categoria[escolha]
     if (escolha === -1) {
         console.clear();
         console.log('Voce cancelou a compra ' + nome, '!');
@@ -23,26 +25,48 @@ if (readline.keyInYN('Voce deseja comprar algum de nossos produtos?')) {
         const produtos_escolhidos = escolha1[escolha];
         const escolha_final = readline.keyInSelect(produtos_escolhidos, "Qual voce deseja comprar?");
         if (escolha_final === -1) {
-            console.clear();;
+            console.clear();
             console.log('Voce cancelou a compra ' + nome, '!');
             return;
         }
         console.clear();
         const continuar = readline.keyInYN('Voce escolheu ' + produtos_escolhidos[escolha_final] + ' deseja continuar?');
         if (continuar === false) {
-            console.clear();;
+            console.clear();
             console.log('Reinicie o programa e escolha outro produto!');
             return;
         }
         console.clear();
         const quantidade = readline.questionInt('Voce deseja quantos?');
         if (quantidade <= 0) {
-            console.clear();;
+            console.clear();
             console.log('Voce inseriu um valor inválido. Saindo...');
             return;
         }
         console.clear();
         console.log('Voce comprou ' + quantidade + ' ' + produtos_escolhidos[escolha_final] + ' obrigado pela compra ' + nome + '!');
+        
+        const historico_De_Compra = {
+            nome: nome,
+            produto: produtos_escolhidos[escolha_final],
+            categoria: categoria_escolha,
+            quantidade: quantidade,
+            data: new Date().toISOString()
+        };
+        let historico = [];
+        if (fs.existsSync('historico.json')) {
+            const dados = fs.readFileSync('historico.json');
+            try {
+                historico = JSON.parse(dados);
+                if (!Array.isArray(historico)){
+                    historico = [];
+                }
+            } catch(e) {
+                historico = [];
+            }
+        }  
+        historico.push(historico_De_Compra); 
+        fs.writeFileSync('historico.json', JSON.stringify(historico, null, 2))     
     }
 } else {
     console.log('Compra cancelada e/ou valor inválido. Fechando programa...');
